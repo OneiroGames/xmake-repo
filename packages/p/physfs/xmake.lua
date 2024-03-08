a67d6adecfc7b9052a4b6b42420bf94b5b9167b4
Nikita Gordeev <dezlowng@gmail.com>
2024-03-08 12:03:22 +0700

    Updated xmake.lua in physfs
    - fix

--- packages/p/physfs/xmake.lua
+++ packages/p/physfs/xmake.lua
@@ -14,9 +14,9 @@ package("physfs")
         local configs = {}
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DPHYSFS_BUILD_DOCS=OFF"))
-        table.insert(configs, "-DPHYSFS_DISABLE_INSTALL=ON"))
-        table.insert(configs, "-DPHYSFS_BUILD_TEST=OFF"))
+        table.insert(configs, "-DPHYSFS_BUILD_DOCS=OFF")
+        table.insert(configs, "-DPHYSFS_DISABLE_INSTALL=ON")
+        table.insert(configs, "-DPHYSFS_BUILD_TEST=OFF")
         if package:is_plat("windows") then
             table.insert(configs, "-DUSE_MSVC_RUNTIME_LIBRARY_DLL=" .. (package:config("vs_runtime"):startswith("MT") and "OFF" or "ON"))
         end
